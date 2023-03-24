c11ecb4b3c39997819830aa7724187e3f995b1a3
Cedric Baude <baude.cedric.perso@gmail.com>
2023-03-24 18:01:44 +0100

    add routing

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -1,4 +1,10 @@
 import React from 'react';
+import { useState } from 'react';
+import { ProSidebar, Menu, MenuItems } from 'react-pro-sidebar';
+import 'react-pro-sidebar/dist/css/styles.css';
+import { Box, IconButton, Typography, useTheme } from '@mui/icons-material';
+import { Link } from 'react-router-dom';
+import { tokens } from '../../theme';
 
 const Sidebar = () => {
     return (
