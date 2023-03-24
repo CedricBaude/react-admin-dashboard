a799aea2c6d6c6b37902a2e70010c94603705b8c
Cedric Baude <baude.cedric.perso@gmail.com>
2023-03-24 22:51:37 +0100

    update

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -5,12 +5,33 @@ import 'react-pro-sidebar/dist/css/styles.css';
 import { Box, IconButton, Typography, useTheme } from '@mui/icons-material';
 import { Link } from 'react-router-dom';
 import { tokens } from '../../theme';
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import PeopleOutlinedIcon from '@mui/icons-material/PeopleOutlined';
+import ContactsOutlinedIcon from '@mui/icons-material/ContactsOutlined';
+import ReceipOutlinedIcon from '@mui/icons-material/ReceipOutlined';
+import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
+import CalendarTodayOutlinedIcon from '@mui/icons-material/CalendarTodayOutlined';
+import HelpOutlinedIcon from '@mui/icons-material/HelpOutlined';
+import BarChartOutlinedIcon from '@mui/icons-material/BarChartOutlined';
+import PieChartOutlineOutlinedIcon from '@mui/icons-material/PieChartOutlineOutlined';
+import TimelineOutlinedIcon from '@mui/icons-material/TimelineOutlined';
+import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
+import MapOutlinedIcon from '@mui/icons-material/MapOutlined';
+
 
 const Sidebar = () => {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+    const [isCollapsed, setIsCollapsed] = useState(false);
+    const [selected, setSelected] = useState("Dashboard");
     return (
-        <div>
-            Sidebar
-        </div>
+        <Box sx={{
+            "& .pro-sidebar-inner": {
+                background: `${colors.primary[400]} !important`
+            },
+        }}>
+
+        </Box>
     );
 };
 
