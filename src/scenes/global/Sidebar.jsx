202978795bf2463c2615e9bcbb713876b60277fc
Cedric Baude <baude.cedric.perso@gmail.com>
2023-03-30 13:00:41 +0200

    sidebar update

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { useState } from 'react';
-import { ProSidebar, Menu, MenuItems } from 'react-pro-sidebar';
-import 'react-pro-sidebar/dist/css/styles.css';
-import { Box, IconButton, Typography, useTheme } from '@mui/icons-material';
+import { ProSidebar, Menu, MenuItems, MenuItem } from 'react-pro-sidebar';
+import "react-pro-sidebar/dist/css/styles.css";
+import { Box, IconButton, Typography, useTheme } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { tokens } from '../../theme';
 import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
 import PeopleOutlinedIcon from '@mui/icons-material/PeopleOutlined';
 import ContactsOutlinedIcon from '@mui/icons-material/ContactsOutlined';
-import ReceipOutlinedIcon from '@mui/icons-material/ReceipOutlined';
+import ReceiptOutlinedIcon from '@mui/icons-material/ReceiptOutlined';
 import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
 import CalendarTodayOutlinedIcon from '@mui/icons-material/CalendarTodayOutlined';
 import HelpOutlinedIcon from '@mui/icons-material/HelpOutlined';
@@ -18,6 +18,16 @@ import TimelineOutlinedIcon from '@mui/icons-material/TimelineOutlined';
 import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
 import MapOutlinedIcon from '@mui/icons-material/MapOutlined';
 
+const Item = ({ title, to, icon, selected, setSelected }) => {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+    return (
+        <MenuItem active={selected === title} style={{ color: colors.grey[100] }} onClick={() => setSelected(title)} icon={icon}>
+            <Typography>{title}</Typography>
+            <Link />
+        </MenuItem>
+    )
+}
 
 const Sidebar = () => {
     const theme = useTheme();
@@ -29,9 +39,46 @@ const Sidebar = () => {
             "& .pro-sidebar-inner": {
                 background: `${colors.primary[400]} !important`
             },
+            "& .pro-icon-wrapper": {
+                backgroundColor: "transparent !important"
+            },
+            "& .pro-inner-item": {
+                padding: "5px 35px 5px 20px !important"
+            },
+            "& .pro-inner-item:hover": {
+                color: "#868dfb !important"
+            },
+            "& .pro-menu-item.active": {
+                color: "#6870fa !important"
+            }
         }}>
 
+            { /* USER */}
+            {!isCollapsed && (
+                <Box mb="25px">
+                    <Box display="flex" justifyContent="center" alignItems="center">
+                        <img
+                            alt="profile-user"
+                            width="100px"
+                            height="100px"
+                            src={`../../assets/user.png`}
+                            style={{ cursor: "pointer", borderRadius: "50%" }} />
+                    </Box>
+                    <Box textAlign="center">
+                        <Typography variant="h2" color={colors.grey[100]} fontWeight="bold" sx={{ m: "10px 0 0 0" }}>Ed Roh</Typography>
+                        <Typography variant="h5" color={colors.greenAccent[500]}>VP Fancy Admin</Typography>
+                    </Box>
+                </Box>
+            )}
+
+            { /* MENU ITEMS */}
+            <Box padding={isCollapsed ? undefined : "10%"}>
+
+            </Box>
+
+
         </Box>
+
     );
 };
 
